c2f2b8dff74de108fb27d96ad74aea1c1c7aa2f3
강동현 <peterkang98@gmail.com>
2025-11-24 10:11:33 +0900

    fix: 토큰에서 권한 추출 로직 수정

--- src/main/java/com/early_express/user_service/infrastructure/security/keycloak/KeycloakClientRoleConverter.java
+++ src/main/java/com/early_express/user_service/infrastructure/security/keycloak/KeycloakClientRoleConverter.java
@@ -7,21 +7,18 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeycloakClientRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 	@Override
 	public Collection<GrantedAuthority> convert(Jwt jwt) {
-		Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
-		if (realmAccess == null) return Collections.emptyList();
 
-		Object roles = ((Map<?,?>)realmAccess).get("roles");
+		Object roles = jwt.getClaim("roles");
 		if (!(roles instanceof Collection)) return Collections.emptyList();
 
 		return ((Collection<?>)roles).stream()
 									 .map(Object::toString)
-									 .filter(s -> s.startsWith("ROLE_"))
+									 .filter(role -> role.startsWith("ROLE_"))
 									 .map(SimpleGrantedAuthority::new)
 									 .collect(Collectors.toList());
 	}
