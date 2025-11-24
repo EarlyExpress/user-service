dbd81d0a6d45e1b4aa7fb45d7aa670af020a5af3
강동현 <peterkang98@gmail.com>
2025-11-24 10:03:55 +0900

    refactor: dto에 검증 실패 메시지 추가

--- src/main/java/com/early_express/user_service/presentation/dto/RefreshTokenRequest.java
+++ src/main/java/com/early_express/user_service/presentation/dto/RefreshTokenRequest.java
@@ -3,7 +3,7 @@ package com.early_express.user_service.presentation.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshTokenRequest(
-		@NotBlank
+		@NotBlank(message = "갱신 토큰이 비어있습니다")
 		String refreshToken
 ) {
 }
