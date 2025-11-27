0a804b0fb061c08980e1028d34e5e67ac8235f83
강동현 <peterkang98@gmail.com>
2025-11-27 10:41:59 +0900

    fix: 사용자 정보 조회 내부 경로 생성

--- src/main/java/com/early_express/user_service/presentation/controller/UserInternalController.java
+++ src/main/java/com/early_express/user_service/presentation/controller/UserInternalController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user/internal/all/profile/{id}")
+@RequestMapping("/user/internal/all/profile")
 public class UserInternalController {
 	private final UserProfileService userProfileService;
 
