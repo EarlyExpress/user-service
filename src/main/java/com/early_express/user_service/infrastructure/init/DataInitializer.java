4bb4d42c541e75f29662561798652adcb390fa2e
강동현 <peterkang98@gmail.com>
2025-11-27 18:11:58 +0900

    chore: 초기 데이터 넣기

--- src/main/java/com/early_express/user_service/infrastructure/init/DataInitializer.java
+++ src/main/java/com/early_express/user_service/infrastructure/init/DataInitializer.java
@@ -36,6 +36,34 @@ public class DataInitializer implements CommandLineRunner {
 		master.approveSignup(Role.MASTER);
 		userRepository.save(master);
 
+		// 허브 담당자
+		User hubManager2 = User.builder()
+							   .keycloakId("d5fec94a-606c-4085-b323-493e9f32c724")
+							   .username("gyeonggi1")
+							   .email("gyeonggi.hub@earlyexpress.com")
+							   .name("이경기")
+							   .hubId("1") // 경기 남부 센터
+							   .companyId(null)
+							   .slackId("slack002")
+							   .build();
+		hubManager2.approveSignup(Role.HUB_MANAGER);
+		userRepository.save(hubManager2);
+
+		// 업체 담당자
+		User seller1Manager = User.builder()
+								  .keycloakId("bdaba9b4-14a4-4b98-83d5-b1b0d7e6a44c")
+								  .username("hankook1")
+								  .email("manager@hankookfood.com")
+								  .name("한국식")
+								  .hubId(null)
+								  .companyId("2a6d1bdc-41a8-4da4-a575-4da213804fd3")
+								  .slackId("slack001")
+								  .build();
+		seller1Manager.approveSignup(Role.COMPANY);
+		userRepository.save(seller1Manager);
+
+
+
 		// 초기화 후 임시 인증 제거
 		SecurityContextHolder.clearContext();
 	}
